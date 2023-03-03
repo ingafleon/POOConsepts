702b48b63705e5fef6fe863c61563e0f88211f23
Alexander Fernando León Díaz <ing.afleon@hotmail.com>
2023-03-03 13:53:42 -0500

    Finish Date Class

---
+++ POOConsepts/POOConsepts/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOConsepts;
+
+Console.WriteLine("POO Consepts!");
+Console.WriteLine("============");
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1978, 10, 22));
+	Console.WriteLine(new Date(1999, 9, 16));
+	Console.WriteLine(new Date(2011, 6, 29));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
+
